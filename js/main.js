dd824d532e1d6915433b033f426e1005449588ca
glitzerbargeld <torben.jaeckel@me.com>
2022-07-18 18:25:02 +0200

    added all the code

---
+++ js/main.js
@@ -0,0 +1,25 @@
+(function($){
+ 
+    "use strict";
+$('form[name="product-search"]').each(function(){
+ 
+        var form          = $(this),
+            search        = form.find('.search'),
+            category      = form.find('.category'),
+            currentQuery  = '',
+            timeout       = false;
+ 
+        category.on('change',function(){
+            currentQuery  = '';
+            var query = search.val();
+            productSearch(form,query,currentQuery,timeout);
+        });
+ 
+        search.keyup(function(){
+            var query = $(this).val();
+            productSearch(form,query,currentQuery,timeout);
+        });
+ 
+    });
+ 
+})(jQuery);
